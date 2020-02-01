1e9b4ef01f4773cfeae6c227b60b3b8675a2e7b5
Paula Banks <pbanks@paulas-mbp.lan>
2020-02-01 15:02:18 +0000

    Building the dev environment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Import any methods from the
+ReactDOM.render(<h2>Hello</h2>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
